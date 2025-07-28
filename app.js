55dd1bdf168bad66c2ce5e8c89f3ecc8f8395f3d
vijay Raja <vijay.raja@rrd.com>
2025-07-28 10:57:48 +0530

    changes

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(bodyParser.json());
 
 app.use('/api/orders', ordersRoutes);
 app.use('/api/fields', fieldsRoutes);
+
 app.use('/api/teacher', teacherRoutes);
 app.use('/api', usersRoutes);
 
